b90062307adad656cdfa45fe661e14e6288aca50
MikeTyson94 <0641659@my.scccd.edu>
2024-12-05 10:36:21 -0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+//MT 9/10/24
+
+
+public class Student {
+    //Give your student class a few fields
+    String firstName;
+    String lastName;
+    String major;
+    double gpa;
+    int age;
+    boolean onProbation;
+}
